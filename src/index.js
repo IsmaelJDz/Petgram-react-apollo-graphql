4cfeca7641f305408b01858c1d7d0b898cb98b49
IsmaelJdz <ismael_br7@hotmail.com>
2019-10-29 17:28:00 -0600

    Basic Files Petgram

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import reactDom from "react-dom";
+
+const App = document.getElementById("app");
+reactDom.render(<h4>Seguimos con el curso avanzado</h4>, App);
+//EAqScZwTvdUM0ByjfJauy88v
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlwebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    filename: "app.bundle.js"
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      template: "src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+}
+
