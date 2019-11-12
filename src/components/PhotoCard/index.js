e64b013bff1553c79390c56e858d1c6fc72c5471
IsmaelJdz <ismael_br7@hotmail.com>
2019-11-11 18:32:36 -0600

    button Like ok

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useRef, useState } from "react";
 import { ImgWrapper, Img, Button, Article } from "./styles";
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 const DEFAUL_IMAGE =
   "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
@@ -11,21 +11,41 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAUL_IMAGE }) => {
   //como prop se tiene que poner ref obligatorio
 
   const [show, setShow] = useState(false);
+  const key = `like-${id}`;
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key);
+      return like;
+    } catch (error) {
+      return false;
+    }
+  });
 
   useEffect(
     function() {
-      const observer = new window.IntersectionObserver(function(entries) {
-        const { isIntersecting } = entries[0];
-        if (isIntersecting) {
-          console.log("si");
-          setShow(true);
-          observer.disconnect();
-        }
+      import("intersection-observer").then(() => {
+        const observer = new window.IntersectionObserver(function(entries) {
+          const { isIntersecting } = entries[0];
+          if (isIntersecting) {
+            setShow(true);
+            observer.disconnect();
+          }
+        });
+        observer.observe(refElement.current);
       });
-      observer.observe(refElement.current);
     },
     [refElement]
   );
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value);
+      setLiked(value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
@@ -38,8 +58,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAUL_IMAGE }) => {
               </ImgWrapper>
             </a>
 
-            <Button>
-              <MdFavoriteBorder size="32px" />
+            <Button onClick={() => setLocalStorage(!liked)}>
+              <Icon size="32px" />
               {likes} likes!
             </Button>
           </Fragment>
